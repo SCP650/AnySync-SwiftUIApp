78ba078243436e184cc0a52f85e2cbc8f1284656
Sebastian Yang <yukaiy@andrew.cmu.edu>
2021-03-05 20:56:02 -0500

    Base Code

---
+++ AnySync/AnySyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnySyncApp.swift
+//  AnySync
+//
+//  Created by Atlas on 3/5/21.
+//
+
+import SwiftUI
+
+@main
+struct AnySyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
