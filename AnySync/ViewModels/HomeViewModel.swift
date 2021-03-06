dc8b14faef92df89b0efbb3505bdf5636c3beae4
Kevin Wang <kevin.wang11@outlook.com>
2021-03-06 16:23:19 -0500

    Added homeviewmodel

---
+++ AnySync/ViewModels/HomeViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  HomeViewModel.swift
+//  AnySync
+//
+//  Created by Atlas on 3/6/21.
+//
+
+import Foundation
+
+class HomeViewModel {
+    @State var buttons: SyncButton = []
+    
+    // Upstream
+    func sendNewButton {
+        
+    }
+    
+    func sendButtonPush {
+        
+    }
+    
+    // Downstream
+    func subscribeToButton {
+        
+    }
+}
