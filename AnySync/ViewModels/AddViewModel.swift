9d547525d1ac9a5d0e748b6c22bb6d85cf770676
Kevin Wang <kevin.wang11@outlook.com>
2021-03-06 18:41:44 -0500

    Added comments to Add View model and home view model

---
+++ AnySync/ViewModels/AddViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  AddViewModel.swift
+//  AnySync
+//
+//  Created by Kevin Wang on 3/6/21.
+//
+
+import SwiftUI
+
+class AddViewModel {
+    
+    // MARK: Form element states
+    @State var buttonName = ""
+    @State var discoverableTime = 0
+    
+    // MARK: New Button
+    func createNewButton() {
+        // TODO: Three things
+        // 1. create a new button document under firebase
+        // 2. create a new subscription in new button document created
+        // 3. get button document and call the persistence function in HomeVM to save to local
+        
+    }
+}
