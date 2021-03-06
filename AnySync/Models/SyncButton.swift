7633b29427f13e770fbb39d305da54ecb9124894
Kevin Wang <kevin.wang11@outlook.com>
2021-03-06 01:46:58 -0500

    Added some models; created home view

---
+++ AnySync/Models/SyncButton.swift
@@ -0,0 +1,14 @@
+//
+//  SyncButton.swift
+//  AnySync
+//
+//  Created by Kevin Wang on 3/6/21.
+//
+
+import Foundation
+
+struct SyncButton: Identifiable, Codable {
+    var id = "ascklasjh1"
+    var name = "Sex"
+    var timeInterval = 3600.0
+}
--- AnySync/Views/HomeView/HomeView.swift
+++ AnySync/Views/HomeView/HomeView.swift
@@ -8,13 +8,32 @@
 import SwiftUI
 
 struct HomeView: View {
+    // TODO: replace data source with actual data source
+    let data = [SyncButton(id: "scasc", name: "Sex", timeInterval: 3600.0), SyncButton(id: "a", name: "Play COD", timeInterval: 1800.0),
+        SyncButton(id: "c", name: "Dinner Date", timeInterval: 1800.0)]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+                List(data, id: \.id) { rowButton in
+                   HomeRowView(syncButton: rowButton)
+                    .padding(.vertical)
+                }
+                .toolbar(content: {
+                    Button(action: {}, label: {
+                        Image(systemName: "plus")
+                            .font(Font.title.weight(.semibold))
+                    })
+                })
+                .navigationTitle("Syncs")
+                .buttonStyle(PlainButtonStyle())
+            }
+            
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .preferredColorScheme(.dark)
     }
 }
